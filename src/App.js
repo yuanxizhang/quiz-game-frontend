8ee0ef07b81d710c421faf0ca7a4c88f9cb97190
yuanxizhang <yuanxi.zhang@gmail.com>
2020-11-18 23:33:01 -0800

    Add FlashcardContainer.js and container.css

--- src/App.js
+++ src/App.js
@@ -1,55 +1,14 @@
-import React, {useState, useEffect, useRef} from 'react';
-import axios from 'axios'
-import { BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
-import TestsContainer from './containers/TestsContainer';
-import QuestionsContainer from './containers/QuestionsContainer';
-import QuizGame from './components/questions/QuizGame';
+import React from 'react';
+import { Router, Switch, Route, Link} from 'react-router-dom';
+import FlashcardsContainer from './containers/FlashcardsContainer';
 import history from './services/history';
 import Login from './pages/Login';
-import SignUp from './pages/SignUp';
-import FlashcardList from './FlashcardList' 
+import SignUp from './pages/SignUp'; 
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import "./App.css";
 
-function App() {
+const App = () => {
 
-  const [flashcards, setFlashcards] = useState([]);
-  const [subjects, setSubjects] = useState([])
-
-  const subjectEl = useRef()
-  const baseUrl = 'http://online-quiz-api.herokuapp.com/api/v1/';
-
-  useEffect(() => {
-    axios
-      .get(`${baseUrl}/tests`)
-      .then(resp => {
-        setSubjects(resp.data)
-      })
-  }, [])
-
-  useEffect(() => {
-   
-  }, [])
-
-  function handleSubmit(e) {
-    e.preventDefault()
-    axios
-    .get(`${baseUrl}/tests`)
-    .then((resp) => {
-      setFlashcards(resp.data[subjectEl.current.value-1].questions
-      .map((questionItem, index) => {
-        return {
-          id: questionItem.id,
-          question: questionItem.question,
-          answer: questionItem.answer,
-          explain: questionItem.explain,
-          options: questionItem.options
-        }       
-      }))
-    })
-  }
-  
-  
   return ((
     <Router history={history}>
 	  	<div className="App">
@@ -61,28 +20,11 @@ function App() {
                   <li><Link to="/signup">Sign up</Link></li>
                </ul>
             </div>
-        </nav>
-
-        <form className="header" onSubmit={handleSubmit}>
-          <div className="form-group">
-            <label htmlFor="subject">Select topic:</label>
-            <select id="subject" ref={subjectEl}>
-              {subjects.map(subject => {
-                return <option value={subject.id} key={subject.id}>{subject.name}</option>
-              })}
-            </select>
-          </div>
-          
-          <div className="form-group">
-            <button className="btn">Get</button>
-          </div>
-        </form>
+        </nav>     
 	  		
         <div className="main">
           <Switch>
-            <Route exact path='/' render={() => (
-                  <FlashcardList flashcards={flashcards} />
-            )} />
+            <Route exact path='/' component={FlashcardsContainer} />
             <Route path="/login" component={Login} />
             <Route path="/signup" component={SignUp} />
           </Switch> 
---
+++ src/containers/FlashcardsContainer.js
@@ -0,0 +1,68 @@
+import React, {useState, useEffect, useRef} from 'react';
+import axios from 'axios'
+import FlashcardList from '../FlashcardList' 
+import "./container.css";
+
+const FlashcardsContainer = () => {
+    const [flashcards, setFlashcards] = useState([]);
+    const [subjects, setSubjects] = useState([]);
+
+    const subjectEl = useRef()
+    const baseURL = 'http://localhost:5000/api/v1/';
+
+    useEffect(() => {
+        axios
+        .get(`${baseURL}/tests`)
+        .then(resp => {
+            setSubjects(resp.data)
+        })
+        .catch((error) => {
+            console.log('Error', error.message);
+        })
+            
+    }, [])
+
+    useEffect(() => {
+    
+    }, [])
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        axios
+        .get(`${baseURL}/tests`)
+        .then((resp) => {
+            console.log(resp.data.filter(s => s.name === subjectEl.current.value)[0]);
+            setFlashcards(resp.data.filter(s => s.name === subjectEl.current.value)[0].questions);
+        })
+        .catch((error) =>  {
+            console.log('Error', error.message);
+        })
+    }
+
+    return (
+       <div>
+        <div>
+            <form className="header form-inline" onSubmit={handleSubmit}>
+                <div className="form-group row">
+                    <label htmlFor="subject" className="col-form-label col-sm-10">Select a subject:</label>
+                    <select id="subject" className ="custom-select col-sm-10" ref={subjectEl}>
+                    {subjects.map(subject => {
+                        return <option value={subject.name} key={subject.id}>{subject.name}</option>
+                    })}
+                    </select>
+                </div>
+                
+                <div className="form-group form-inline">
+                    <button className="btn btn-info">Get Flashcards</button>
+                </div>
+            </form>
+        </div>
+        <div className="main">
+            <FlashcardList flashcards={flashcards} />
+        </div> 
+      </div>
+    );
+    
+}
+
+export default FlashcardsContainer;

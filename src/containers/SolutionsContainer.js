e57dfdfeca443b67c772cb312d6d700e714c6ad6
yuanxizhang <yuanxi.zhang@gmail.com>
2020-12-12 13:04:28 -0800

    Update SolutionsContainer

--- src/containers/SolutionsContainer.js
+++ src/containers/SolutionsContainer.js
@@ -1,148 +1,32 @@
-import React, { useState } from "react";
-import TestDataService from "../services/TestDataService";
-
-const NewTest = () => {  
-  const [test, setTest] = useState(initialTestState);
-  const [questions, setQuestions] = useState(emptyQuestion);
-  const [submitted, setSubmitted] = useState(false);
-
-
-  const initialTestState = {
-    id: null,
-    mame: '',
-    questions_attributes: [Object.assign({}, this.emptyQuestion)]
-  };
-
-  const emptyQuestion = {  
-    id: null,
-    question: '',
-    answer: '',
-    explain: '',
-    options_attributes: [Object.assign({}, this.emptyOption)]
-  };
-
-  const emptyOption = { 
-    id: null,
-    item: ''
-  };  
-
-  const handleInputChange = event => {
-    const { name, value } = event.target;
-    setTest({ ...test, [name]: value });
-  };
-
-  const saveTest = () => {
-    var data = {
-      name: test.name,
-      questions: test.questions
-    };
-
-    TestDataService.create(data)
-      .then(response => {
-        setTest({
-          id: response.data.id,
-          name: response.data.name,
-          questions: response.data.questions
-        });
-        setSubmitted(true);
-        console.log(response.data);
-      })
-      .catch(e => {
-        console.log(e);
-      });
-  };
-
-  const newTest = () => {
-    setTest(initialTestState);
-    setSubmitted(false);
-  };
-
-  const renderQuestionsForm = () => {
-    let counter = 0;
-
-    function handleRemoveQuestion(id) {
-        const newQuestions = questions.filter((question) => question.id !== id);    
-        setQuestions(newQuestions);
-    }
-    return test.questions_attributes.map((question, index) => {
-      
-        let questionDOM = (
-          <div className="question-form" key={index}>
-            <div className="form-group">
-              <div className="clearfix" style={{ marginBottom: 5 }}>
-                <label>
-                  Question {counter + 1}
-                </label>
-                <button
-                  className="btn btn-danger"
-                  style={{ padding: '5px 10px', float: 'right' }}
-                  onClick={handleRemoveQuestion}>
-                  X
-                </button>
-              </div>
-              <input
-                placeholder="Question"
-                onChange={handleInputChange}
-                type="text"
-                value={question.question}
-                className="form-control"
-              />
-              
-            </div>
-          </div>
-        );
-        counter++;
-  
-        return questionDOM;
-      }
+import React, { Component } from 'react'
+import SolutionInput from '../components/solutions/SolutionInput'
+import Solutions from '../components/solutions/Solutions'
+import {connect} from 'react-redux'
+class SolutionsContainer extends Component {
+  render() {
+    return (
+      <div>
+        <SolutionInput
+          addSolution={this.props.addSolution}
+          restaurantId={this.props.restaurant.id}
+        />
+        <Solutions
+          solutions={this.props.solutions}
+          restaurantId={this.props.restaurant.id}
+          deleteSolution={this.props.deleteSolution}
+        />
+      </div>
     );
   }
-  
+}
 
-  return (
-    <div className="submit-form">
-      {submitted ? (
-        <div>
-          <h4>You submitted successfully!</h4>
-          <button className="btn btn-success" onClick={newTest}>
-            New Quiz
-          </button>
-        </div>
-      ) : (
-        <div>
-          <div className="form-group">
-            <label htmlFor="name">Name</label>
-            <input
-              type="text"
-              className="form-control"
-              id="name"
-              required
-              value={test.name}
-              onChange={handleInputChange}
-              name="name"
-            />
-          </div>
+const mapStateToProps = ({solutions}) => {
+  return {solutions}
+}
 
-          
-        <hr />
-        <div className="questions-fieldset">
-          <h3>Questions</h3>
-          {renderQuestionsForm()}
-          <button
-            className="btn btn-success"
-            onClick={e => handleAddQuestion()}>
-            + Add Question
-          </button>
-        </div>
-        <br />
+const mapDispatchToProps = dispatch => ({
+  addSolution: solution => dispatch({type: 'ADD_SOLUTION', solution}),
+  deleteSolution: id => dispatch({type: 'DELETE_SOLUTION', id})
+})
 
-          <button onClick={saveTest} className="btn btn-success">
-            Submit
-          </button>
-        </div>
-      )}
-    </div>
-  );
-};
-
-export default NewTest;
+export default connect(mapStateToProps, mapDispatchToProps)(SolutionsContainer)

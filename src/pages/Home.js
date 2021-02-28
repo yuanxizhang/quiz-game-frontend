415c8b1ffd37cb7cbc1a823a85775059575209b2
yuanxizhang <yuanxi.zhang@gmail.com>
2021-02-27 20:58:26 -0800

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,7 +6,7 @@ const Home = () => {
     const slides = [
         { title: 'Step One', description: 'Be Clear About Your End Goal and Commit to It', image: "https://i.imgur.com/bRJ9Eki.jpeg"},
         { title: 'Step Two', description: 'Select A Language to Learn', image: "https://i.imgur.com/DvmN8Hx.jpg"},
-        { title: 'Step Three', description: 'Read Code Written by Experienced Developers', image: "https://i.imgur.com/s1BaPrn.png"},
+        { title: 'Step Three', description: 'Read Code Written by Experienced Developers', image: ""},
         { title: 'Step Four', description: 'Join A Community of Software Engineers', image: "https://i.imgur.com/QBgYHvG.jpeg"},
         { title: 'Step Five', description: 'Build projects', image: "https://i.imgur.com/bMOScIo.jpeg"},
         { title: 'Step Six', description: 'Ace your technical interview', image: "https://i.imgur.com/t3z4KcD.jpeg"}
